1a5e0538efae3438b23798f54723b5881ddb70a3
datariomj <mail@datariomj.dev>
2024-04-10 02:23:10 +0800

    feature: dynamic robots.txt file

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  version: '0.1.0',
+  hostUrl: 'https://datariomj.dev',
+  contentful: {
+    host: 'https://cdn.contentful.com',
+    hostGraphQL: 'https://graphql.contentful.com',
+    environment: 'master', // todo implement staging environment
+    space: 's5tpeora8n1x',
+    accessToken: 'dTTJKMDYBBneMpjykSXggLlZTBYn004i_AUlRFvmScc', // read only
+  },
+  facebook: {
+    appId: '2167184696673703',
+  },
+};
